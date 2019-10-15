e633ae22c2e23d461bbc65725872ff3f32ab4410
Emil6677 <rene.boe1@freenet.de>
2019-10-15 14:14:29 +0200

    Automatisch js html sass aber noch keine jquery atomatisch

--- gulpfile.js
+++ gulpfile.js
@@ -1,85 +1,99 @@
-var gulp          = require('gulp'),
-    sass          = require('gulp-sass'),
-    cleanCSS      = require('gulp-clean-css'),
-    autoprefixer  = require('gulp-autoprefixer'),
-    rename        = require('gulp-rename'),
-    inject        = require('gulp-inject'),
-    uglify        = require('gulp-uglify'),
-    plumber       = require('gulp-plumber'),
-    browserify    = require('gulp-browserify'),
-    clean         = require('gulp-clean'),
-    sourcemaps    = require('gulp-sourcemaps'),
-    htmlmin       = require('gulp-html-minifier'),
-    browserSync   = require('browser-sync'),
-    babel         = require('gulp-babel'),
-    concat        = require('gulp-concat'),
-    minify        = require('gulp-minify'),
-    watch         = require('gulp-watch')
-    ;
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  cleanCSS = require('gulp-clean-css'),
+  autoprefixer = require('gulp-autoprefixer'),
+  rename = require('gulp-rename'),
+  inject = require('gulp-inject'),
+  uglify = require('gulp-uglify'),
+  plumber = require('gulp-plumber'),
+  browserify = require('gulp-browserify'),
+  clean = require('gulp-clean'),
+  sourcemaps = require('gulp-sourcemaps'),
+  htmlmin = require('gulp-html-minifier'),
+  browserSync = require('browser-sync'),
+  babel = require('gulp-babel'),
+  concat = require('gulp-concat'),
+  minify = require('gulp-minify'),
+  watch = require('gulp-watch');
 
-var src        ='./src/',
-    dist       ='./dist/';
+var src = './src/',
+  dist = './dist/';
 
 
-    // ###########################################################
-    // MINIFY SASS
+// ###########################################################
+// MINIFY SASS
 
-    gulp.task('sass', done => {
-      gulp.src(src + 'assets/sass/*.sass')
-        .pipe(sourcemaps.init())
-          .pipe(plumber())
-          .pipe(sass())
-          .pipe(autoprefixer())
-          .pipe(rename({ basename: 'style'}))
-          .pipe(cleanCSS())
-          .pipe(rename({ suffix: '.min'}))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(dist + 'assets/css'))
-        done();
-    });
+gulp.task('sass', done => {
+  gulp.src(src + 'assets/sass/*.sass')
+    .pipe(sourcemaps.init())
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(rename({
+      basename: 'style'
+    }))
+    .pipe(cleanCSS())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(dist + 'assets/css'))
+  done();
+});
 
+// ###########################################################
+// MINIFY JS
+
+gulp.task('js',done => {
+  gulp.src(src + 'assets/js/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(plumber())
+    .pipe(concat('global.js'))
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    /*.pipe(browserify({
+      insertGlobals : true,
+      debug : !gulp.env.production
+    }))*/
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(dist + 'assets/js'))
+    done();
+});
+
+
+/*gulp.task('browserify', function() {
+  return browserify(sourceFile)
+  .bundle({debug:true})
+  .pipe(source(destFile))
+  .pipe(gulp.dest(destFolder));
+});*/
 
 // ###########################################################
 // MINIFY HTML
 
 gulp.task('html', done => {
-  gulp.src(dist + '*.html',{force: true})
+  gulp.src(dist + '*.html', {
+      force: true
+    })
     .pipe(clean());
   gulp.src(src + '*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
     .pipe(gulp.dest(dist))
-    done();
+  done();
 });
 
-
-
-// ###########################################################
-// MINIFY js
-
-gulp.task('js', done => {
-  gulp.src('./app/js/*.js')
-    .pipe(babel({presets: ['@babel/env'] }))
-    .pipe(concat('global.js'))
-    .pipe(gulp.dest('./src/js/dist'))
-    .pipe(minify())
-    .pipe(gulp.dest('./src/js/dist'))
-    done();
-});
 
 // ###########################################################
 // WATCH
 
-/*gulp.task('default', () => {
-  gulp.watch(['./app/sass/*.sass','./app/js/*.js'],
-  gulp.parallel('sass','js','html'));
-});*/
-
-
-gulp.task('default', () => {
-  gulp.watch([src + '*.html']);
-  gulp.watch([src + 'assets/sass/*.sass']);
-  gulp.series('sass','html');
-
-  //,'./app/js/*.js'],
-  //gulp.parallel('html','sass','js'));
+gulp.task('default', function() {
+  gulp.watch([src + '*.html', src + 'assets/sass/*.sass', src + 'assets/js/*.js'],
+    gulp.parallel('html', 'sass', 'js'));
 });
--- src/js/dist/global-min.js
+++
@@ -1,1 +0,0 @@
-"use strict";var s=function(r){return r},z=80,c=4,g=44;
